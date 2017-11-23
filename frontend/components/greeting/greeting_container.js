639f8d6b349e842b58ad6fa93e1bd961457b71d1
JiwooPark <jpark830@me.com>
2017-11-22 21:52:40 -0800

    small bug fixed in greeting container

--- frontend/components/greeting/greeting_container.js
+++ frontend/components/greeting/greeting_container.js
@@ -3,12 +3,10 @@ import { logout } from '../../actions/session_actions';
 import Greeting from './greeting';
 
 const mapStateToProps = (state, ownProps) => {
-  const pathname = ownProps.location.pathname.length > 1 ? 
-    ownProps.location.pathname.slice(1) : 
-    ownProps.location.pathname
+  
   return ({
     currentUser: state.session.currentUser,
-    currentPath: pathname
+  
   })
 };
 
