d195fa0ef9249c1e107e92b4351f38aead525434
JiwooPark <jpark830@me.com>
2017-12-02 20:53:12 -0800

    remove application.yml from .gitignore, add Aws.config.update

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,1 +1,6 @@
+Aws.config.update({
+  region: 'us-west-1',
+  credentials: Aws::Credentials.new(ENV['s3_access_key_id'], ENV['s3_secret_access_key'])
+})
+
 Aws::VERSION = Gem.loaded_specs["aws-sdk"].version
