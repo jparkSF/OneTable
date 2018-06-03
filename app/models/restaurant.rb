29bea7624d9abb6983a6b1fbe59dfb0b0d8ef846
JiwooPark <jpark830@me.com>
2018-06-02 23:45:47 -0700

    change dabase.yml

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -35,7 +35,7 @@ class Restaurant < ApplicationRecord
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "default_restaurant.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/, optional: true
-  
+  # validates_attachment_content_type :attachment, :content_type => /image/, optional: true
 
   belongs_to :owner,
     foreign_key: :owner_id,
