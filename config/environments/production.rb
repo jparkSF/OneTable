44c5e3ecf2db31d79129325c22c00dac7dab1229
JiwooPark <jpark830@me.com>
2018-06-11 13:19:57 -0700

    add public_file_server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   
 
   # Compress JavaScripts and CSS.
@@ -32,6 +32,7 @@ Rails.application.configure do
   config.assets.compile = true
 
   config.serve_static_assets = true
+  config.public_file_server = true
   config.assets.digest = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
